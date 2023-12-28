f7c3f35ef0876088f89fed4807a13f43bf9a9dc3
Gabriel Portela <gabriel.portela@ime.eb.br>
2023-12-28 08:56:42 -0400

    questões

---
+++ catálogo de livros.py
@@ -0,0 +1,14 @@
+por = [int(x) for x in input().split()][1:]
+mat = [int(x) for x in input().split()][1:]
+qui = [int(x) for x in input().split()][1:]
+fis = [int(x) for x in input().split()][1:]
+bio = [int(x) for x in input().split()][1:]
+k = int(input())
+v = []
+for a in por:
+    for b in mat:
+        for c in qui:
+            for d in fis:
+                for e in bio: 
+                    v.append(tuple([a, b, c, d, e]))
+print(sum(sorted([sum(i) for i in v], reverse=True)[:k]))
