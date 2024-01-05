9e6139165c21807304addda8ca3770698ae0c478
Gabriel Portela <gabriel.portela@ime.eb.br>
2024-01-05 13:28:36 -0400

    questoes

--- nomes de usuarios.py
+++ nomes de usuarios.py
@@ -1,19 +1,21 @@
+import os, sys, io
+input = lambda: sys.stdin.readline()[:-1]
+
+def imprimir(texto):
+    sys.stdout.write(str(texto) + "\n")
+
 while True:
-    try:
-        entrada = input().split()
-        alunos = int(entrada[0])
-        ano = entrada[1]
-        users = []
-        quantidade = 0
-        for i in range(alunos):
-            nomes = input().split()
-            user = ""
-            for nome in nomes:
-                user += nome[0]
-            if(user not in users):
-                users.append(user)
-            else:
-                quantidade += 1
-        print(quantidade)
-    except EOFError:
+    entrada = input().split()
+    if not entrada:
         break
+    alunos = int(entrada[0])
+    ano = entrada[1]
+    users = set()
+    quantidade = 0
+    for i in range(alunos):
+        nomes = input().split()
+        user = ""
+        for nome in nomes:
+            user += nome[0]
+        users.add(user)
+    imprimir(alunos - len(users))
