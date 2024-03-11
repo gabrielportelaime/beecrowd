443def4d0556a02ea2e6a92c9b2f678319115aed
Gabriel Portela <gabriel.portela@ime.eb.br>
2024-03-11 12:26:51 -0400

    contest

--- CONTEST933_DIV3/D.py
+++ CONTEST933_DIV3/D.py
@@ -1,5 +1,6 @@
 
 import os, sys, io
+from collections import deque
 input = lambda: sys.stdin.readline()[:-1]
 
 def imprimir(texto):
@@ -7,16 +8,38 @@ def imprimir(texto):
 
 respostas = []
 for _ in range(int(input())):
-    tamanho = int(input())
-    lista = [int(x) for x in input().split()]
-    for i in range(1, tamanho - 1):
-        if(lista[i-1] > 0 and lista[i] > 1 and lista[i+1] > 0):
-            desconto = min(lista[i]//2, lista[i - 1], lista[i + 1])
-            lista[i-1] -= desconto
-            lista[i+1] -= desconto
-            lista[i] -= 2*desconto
-    if(sum(lista) == 0):
-        respostas.append('YES')
-    else:
-        respostas.append('NO')
-imprimir('\n'.join(respostas))
+    jogadores_atual = []
+    jogadores_bola = []
+    jogadores, lancamentos, jogador = [int(x) for x in input().split()]
+    jogadores_atual.append(jogador)
+    for i in range(lancamentos):
+        entrada = input()
+        lancado = int(entrada[:-1:])
+        if(entrada[-1] == '?'):
+            for j in range(len(jogadores_atual)):
+                anti = (jogadores_atual[j] - lancado)%jogadores
+                horario = (jogadores_atual[j] + lancado)%jogadores
+                if(anti == 0):
+                    anti = jogadores
+                if(horario == 0):
+                    horario = jogadores
+                jogadores_bola.append(anti)
+                jogadores_bola.append(horario)
+        elif(entrada[-1] == '1'):
+            for j in range(len(jogadores_atual)):
+                anti = (jogadores_atual[j] - lancado)%jogadores
+                if(anti == 0):
+                    anti = jogadores
+                jogadores_bola.append(anti)
+        elif(entrada[-1] == '0'):
+            for j in range(len(jogadores_atual)):
+                horario = (jogadores_atual[j] + lancado)%jogadores
+                if(horario == 0):
+                    horario = jogadores
+                jogadores_bola.append(horario)
+        jogadores_bola = list(set(jogadores_bola))
+        jogadores_atual = jogadores_bola
+        jogadores_bola = []
+    jogadores_atual.sort()
+    imprimir(len(jogadores_atual))
+    imprimir(' '.join([str(x) for x in jogadores_atual]))
