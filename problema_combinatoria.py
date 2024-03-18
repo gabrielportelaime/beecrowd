07d8c9eff5f69523c85a2180c31d2434a9a0a042
Gabriel Portela <gabriel.portela@ime.eb.br>
2024-03-18 19:01:58 -0400

    doideira

--- problema_combinatoria.py
+++ problema_combinatoria.py
@@ -1,8 +1,10 @@
-contador = 0
-for i in range(1, 101):
-    for j in range(1, 101):
-        for k in range(1, 101):
-            if(i + j + k == 120 and i != j and i != k and j != k):
-                print(i, j, k)
-                contador += 1
-print(contador)
+with open("valores.txt", "w") as arquivo:
+    contador = 0
+    for i in range(1, 101):
+        for j in range(1, 101):
+            for k in range(1, 101):
+                if(i + j + k == 120 and i != j and i != k and j != k):
+                    tripla = '(' + str(i) + ', ' + str(j) + ', ' + str(k) + ')' + '\n'
+                    arquivo.write(tripla)
+                    contador += 1
+    arquivo.write(str(contador))
