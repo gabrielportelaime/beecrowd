0da28a03f8387d1b57500a20cd2e5e15099f982e
Gabriel Portela <gabriel.portela@ime.eb.br>
2024-04-04 12:46:04 -0400

    asdasdasd

--- problema_combinatoria.py
+++ problema_combinatoria.py
@@ -1,10 +1,15 @@
 with open("valores.txt", "w") as arquivo:
     contador = 0
     for i in range(1, 101):
-        for j in range(1, 101):
-            for k in range(1, 101):
+        contador_i = 0
+        for j in range(i + 1, 101):
+            for k in range(j + 1, 101):
                 if(i + j + k == 120 and i != j and i != k and j != k):
                     tripla = '(' + str(i) + ', ' + str(j) + ', ' + str(k) + ')' + '\n'
                     arquivo.write(tripla)
                     contador += 1
+                    contador_i += 1
+        arquivo.write('---------------------------------------------------' + '\n')
+        arquivo.write(str(contador_i) + '\n')
+        arquivo.write('---------------------------------------------------' + '\n')
     arquivo.write(str(contador))
