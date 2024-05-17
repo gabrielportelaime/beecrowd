9bcca0288e9bb6c9c45dcc0c4ea1d031b8b4f6cb
Gabriel Portela <gabriel.portela@ime.eb.br>
2024-05-17 10:06:45 -0400

    Alteração conta dígitos código correto

--- TESTE_MATEMATICA/conta_digitos.py
+++ TESTE_MATEMATICA/conta_digitos.py
@@ -1,5 +1,29 @@
 numeros = []
-n = 7456
+n = 120387
+algarismo = 5
+
 for i in range(n + 1):
     numeros.append(str(i))
-print("".join(numeros).count('1'))
+print("".join(numeros).count(str(algarismo)))
+
+digitos = str(n)
+tamanho = len(digitos)
+qtd_digitos = 0
+sufixo = []
+soma_parcial = 0
+algarismo = 5
+for i in range(tamanho - 1, -1, -1):
+    digito = digitos[i]
+    potencia = tamanho - i
+    if(int(digito) == algarismo):
+        if(len(sufixo) > 0):
+            soma_parcial = (n//(10**potencia))*10**(potencia - 1) + int("".join(sufixo)[::-1]) + 1
+        else:
+            soma_parcial = (n//(10**potencia))*10**(potencia - 1) + 1
+    elif(int(digito) > algarismo):
+        soma_parcial =  (n//(10**potencia) + 1)*10**(potencia - 1)
+    else:
+        soma_parcial =  (n//(10**potencia))*10**(potencia - 1)
+    qtd_digitos += soma_parcial
+    sufixo.append(digito)
+print(qtd_digitos)
--- TESTE_MATEMATICA/conta_digitos_varios.py
+++ TESTE_MATEMATICA/conta_digitos_varios.py
@@ -1,12 +1,29 @@
-import math
 for n in range(1, 1000):
-    soma = 0
-    for i in range(math.floor(math.log10(n)) + 1):
-        soma += (10**i)*(math.floor((n - 10**i)/(10**(i+1))) + 1)
+    digitos = str(n)
+    tamanho = len(digitos)
+    qtd_digitos = 0
+    sufixo = []
+    soma_parcial = 0
+    algarismo = 5
+    for i in range(tamanho - 1, -1, -1):
+        digito = digitos[i]
+        potencia = tamanho - i
+        if(int(digito) == algarismo):
+            if(len(sufixo) > 0):
+                soma_parcial = (n//(10**potencia))*10**(potencia - 1) + int("".join(sufixo)[::-1]) + 1
+            else:
+                soma_parcial = (n//(10**potencia))*10**(potencia - 1) + 1
+        elif(int(digito) > algarismo):
+            soma_parcial =  (n//(10**potencia) + 1)*10**(potencia - 1)
+        else:
+            soma_parcial =  (n//(10**potencia))*10**(potencia - 1)
+        qtd_digitos += soma_parcial
+        sufixo.append(digito)
     numeros = []
     for i in range(n + 1):
         numeros.append(str(i))
-    if(soma == "".join(numeros).count('1')):
-        print(n, soma, "-","".join(numeros).count('1'), "OK")
+    qtd_digitos_correto = "".join(numeros).count(str(algarismo))
+    if(qtd_digitos == qtd_digitos_correto):
+        print(n, qtd_digitos, "-", qtd_digitos_correto, "OK")
     else:
-        print(n, soma, "-","".join(numeros).count('1'), "ERROR")
+        print(n, qtd_digitos, "-", qtd_digitos_correto, "ERROR")
